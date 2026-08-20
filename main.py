32c740f9b43320d6984f3b97f73dc8310a1ab00b
nzifvi <bs478@sussex.ac.uk>
2026-08-20 23:05:05 +0100

    continued encoding questions

--- main.py
+++ main.py
@@ -11,10 +11,36 @@ questionEncodings = {
         "50-59"             : 3,
         "60-69"             : 4,
         "70 years or over"  : 5,
-        "Prefer not to say" : -1
+        "Prefer not to say" : 6
     },
-    "Q2" : {
-        
+    "Q2" : { # what is your gender
+        "Male"                    : 0,
+        "Female"                  : 1,
+        "Non-binary/third gender" : 2,
+        "Prefer not to say"       : 3
+    },
+    "Q3" : {
+        "Brighton and Hove" : 0,
+        "East Sussex"       : 1,
+        "West Sussex"       : 2,
+        "Other"             : 3
+    },
+    "Q4" : { # what is relationship to person you care for
+        "Spouse"              : 0,
+        "Parent"              : 1,
+        "Other family member" : 2,
+        "Friend"              : 3,
+        "Other"               : 4
+    },
+    "Q5" : { # are you currently providing unpaid care for person living w/ dementia
+        "Yes" : 1,
+        "No"  : 0
+    },
+    "Q6" : { # if Q5 = "No" then when did your caring role end
+        "Less than 3 months ago" : 0,
+        "3-6 months"             : 1,
+        "6-12 months"            : 2,
+        "12-24 months"           : 3
     }
 }
 df   = pandas.read_csv("data/dementiaData.csv")
